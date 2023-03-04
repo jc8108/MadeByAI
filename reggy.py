5f97091f44ab333d8956681886f7cce77ce9f702
jc8108 <92411582+jc8108@users.noreply.github.com>
2023-03-03 20:05:00 -0800

    Update reggy.py

--- reggy.py
+++ reggy.py
@@ -1,5 +1,24 @@
 ## create a python program that asks for an example string or strings such as phone number or email address and creates a regex expression from the string that can be used in other applications
 ## TESTED - WORKS, but is a bit too precise sometimes. If you want regex for an IP address and give it 10.10.10.10 it will only give you a regex that matches IP addresses with two digit octets
+# So instead, just asked it for regex of what I wanted.
+
+# write a regular expression that will detect all the different formats a phone number can be in
+# ((\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4})
+
+# write a regular expression that can be used to find addresses
+# \d+\s+\w+\s+\w+,\s*\w+\s*\d+(-\d+)*
+
+# write a regular expression that looks for email addresses
+# \b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b
+
+# write a regular expression that can be used to find domains
+# \b\w+(?:\.\w+)+\b
+
+# write a regular expression that looks for base64 encoded text
+# \b[A-Za-z0-9+/]{4,}(?:=[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?\b
+
+# write a regular expression that looks for strings that are more than 8 characters, have a capital and uncapital letter, a number, and a symbol in them
+# ^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W)[A-Za-z\d\W]{9,}$
 
 import re
 
