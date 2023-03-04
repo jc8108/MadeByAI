629ee9b94db0639a13e8fd934d6e15add9581417
jc8108 <92411582+jc8108@users.noreply.github.com>
2023-03-03 19:57:14 -0800

    Update reggy.py

--- reggy.py
+++ reggy.py
@@ -1,5 +1,5 @@
 ## create a python program that asks for an example string or strings such as phone number or email address and creates a regex expression from the string that can be used in other applications
-## UNTESTED
+## TESTED - WORKS, but is a bit too precise sometimes. If you want regex for an IP address and give it 10.10.10.10 it will only give you a regex that matches IP addresses with two digit octets
 
 import re
 
@@ -23,11 +23,11 @@ regex = "".join(patterns)
 print("Regular expression:")
 print(regex)
 
-Here's how you can use the program:
-
-    Copy the code above into a Python file (e.g. regex_generator.py).
-    Run the script by running python regex_generator.py in your terminal/command prompt.
-    Enter an example string (e.g. a phone number, email address, or any other string with a specific pattern).
-    The script will output a regular expression that matches strings with the same pattern as the example string you entered.
-
-For example, if you enter the string "john@example.com", the script will output the regular expression \\w+@\\w+\\.\\w+. This regular expression matches any string that starts with one or more word characters (\\w+), followed by the "@" symbol, followed by one or more word characters, followed by a period, followed by one or more word characters. So this regular expression can be used to match other email addresses with the same pattern.
+# Here's how you can use the program:
+#
+#    Copy the code above into a Python file (e.g. regex_generator.py).
+#    Run the script by running python regex_generator.py in your terminal/command prompt.
+#    Enter an example string (e.g. a phone number, email address, or any other string with a specific pattern).
+#    The script will output a regular expression that matches strings with the same pattern as the example string you entered.
+#
+# For example, if you enter the string "john@example.com", the script will output the regular expression \\w+@\\w+\\.\\w+. This regular expression matches any string that starts with one or more word characters (\\w+), followed by the "@" symbol, followed by one or more word characters, followed by a period, followed by one or more word characters. So this regular expression can be used to match other email addresses with the same pattern.
